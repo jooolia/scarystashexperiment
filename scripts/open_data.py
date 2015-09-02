6c3516a5beed9c1d46808f7b6980a32e1d1545c2
Bartosz T <bartosz@telenczuk.pl>
2015-09-02 16:56:29 +0200

    merged team awesome

--- scripts/open_data.py
+++ scripts/open_data.py
@@ -10,8 +10,17 @@ if __name__ == "__main__":
 
     data = io.loadmat('../data/data_03_13_12_t2.mat')
     spt = data['spikeTimes']
-    spt1 = spt[0][0]
-    spt2 = spt[0][1]
-    print(analysis.calc_corr_coef(spt1[0,:], spt2[0,:]))
+    n_spt = spt.shape[1]
+    print(spt.shape)
+    correlations = np.zeros((n_spt, n_spt))
+    for i in range(n_spt):
+        for j in range(n_spt):
+            correlations[i,j] = analysis.calc_corr_coef(spt[0][i][0,:], spt[0][j][0,:])
+
+    # plot the histogram
+    plt.hist(correlations.flat, bins=50)
+    plt.xlabel('Correlation value [a.u.]')
+    plt.ylabel('# occurrences')
+    plt.show()
 
 
